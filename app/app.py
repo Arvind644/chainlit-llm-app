fac06ec4fd4b7892ae3653b269bbac095cee3287
Arvind Singharpuria <65646063+Arvind644@users.noreply.github.com>
2024-10-06 16:29:59 +0000

    added base llm

--- app/app.py
+++ app/app.py
@@ -1,16 +1,46 @@
 import chainlit as cl
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.schema import StrOutputParser
+from langchain.chains import LLMChain
+
+@cl.on_chat_start
+async def on_chat_start():
+    model = ChatOpenAI(
+        model="gpt-4o-mini",
+        streaming=True
+    )
+
+    prompt = ChatPromptTemplate.from_messages(
+         [
+            (
+                "system",
+                "You are Chainlit GPT, a helpful assistant.",
+            ),
+            (
+                "human",
+                "{question}"
+            ),
+        ]
+    )
+
+    chain = LLMChain(
+        llm=model,
+        prompt=prompt,
+        output_parser=StrOutputParser()
+    )
+
+    cl.user_session.set("chain", chain)
 
 
-##############################################################################
-# Exercise 1a: 
-# Please add the proper decorator to this main function so Chainlit will call
-# this function when it receives a message
-##############################################################################
+@cl.on_message
 async def main(message: cl.Message):
 
-##############################################################################
-# Exercise 1b:
-# Please get the content of the chainlit Message and send it back as a
-# response
-##############################################################################
-    await cl.Message().send()
+    chain = cl.user_session.get("chain")
+
+    response = await chain.arun(
+        question=message.content,
+        callbacks=[cl.LangchainCallbackHandler()]
+    )
+
+    await cl.Message(content=response).send()
